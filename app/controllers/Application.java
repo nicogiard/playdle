8e31f3216995d5e0baab1cb4fc3673cf66b53903
nicogiard <nicogiard@gmail.com>
2010-10-20 23:16:34 +0200

    ajout de la creation dun event simple

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Valid;
 import play.mvc.*;
 
 import java.util.*;
@@ -9,8 +10,20 @@ import models.*;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		// Only 5 last events
+		List<Event> events = Event.find("").fetch(5);
+		render(events);
+	}
 
+	public static void create(@Valid Event event) {
+		// create the new event
+		if (validation.hasErrors()) {
+			params.flash();
+			validation.keep();
+		} else {
+			event.save();
+		}
+		index();
+	}
 }
