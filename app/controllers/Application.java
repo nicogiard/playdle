941ee3da56f030f9b0de9d9c903547dc35919532
nicogiard <nicogiard@gmail.com>
2010-10-20 23:24:30 +0200

    ajout de date a un event

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
 import play.data.validation.Valid;
 import play.mvc.*;
 
@@ -11,8 +12,8 @@ import models.*;
 public class Application extends Controller {
 
 	public static void index() {
-		// Only 5 last events
-		List<Event> events = Event.find("").fetch(5);
+		// Only 5 last events with status set to true
+		List<Event> events = Event.find("status = true").fetch(5);
 		render(events);
 	}
 
@@ -21,22 +22,61 @@ public class Application extends Controller {
 		if (validation.hasErrors()) {
 			params.flash();
 			validation.keep();
+			index();
 		} else {
 			event.save();
 		}
-		index();
+		date(event.id);
 	}
-	
+
 	public static void events() {
-		// All events
-		List<Event> events = Event.findAll();
+		// All events with status set to true
+		List<Event> events = Event.find("status = true").fetch();
 		render(events);
 	}
 
 	public static void show(Long id) {
-		// show the event identified by id 
+		// show the event identified by id
+		Event event = Event.findById(id);
+		notFoundIfNull(event);
+		if (!event.status) {
+			notFound();
+		}
+		render(event);
+	}
+	
+	public static void date(Long id) {
+		// show the event identified by id in order to add dates
 		Event event = Event.findById(id);
 		notFoundIfNull(event);
 		render(event);
+	}
+
+	public static void addDate(@Required Long id, @Valid @Required Date date) {
+		// Default format for date is yyyy-MM-dd
+		
+		if (validation.hasErrors()) {
+			params.flash();
+			validation.keep();
+		} else {
+			Event event = Event.findById(id);
+			notFoundIfNull(event);
+
+			EventDate eventDate = new EventDate(date);
+			validation.valid(eventDate);
+			if (validation.hasErrors()) {
+				params.flash();
+				validation.keep();
+			}
+			if (!event.listEventDate.contains(eventDate)) {
+				event.listEventDate.add(eventDate);
+				event.save();
+				flash.clear();
+				validation.clear();
+			} else {
+				validation.addError("date", "the date already exist");
+			}
+		}
+		date(id);
 	}
 }
